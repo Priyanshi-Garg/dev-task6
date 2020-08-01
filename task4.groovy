4eae1768cdeac655b11eafaedcad3bef5bf9b4e1
Priyanshi-Garg <64350537+Priyanshi-Garg@users.noreply.github.com>
2020-08-01 22:14:33 +0530

    DSL

---
+++ task4.groovy
@@ -0,0 +1,19 @@
+job("devops_task6_job1"){
+  description("Pull the data from github repo automatically when some developers push code to github")
+  scm{
+    github("","master")
+  }
+  triggers {
+    scm("* * * * *")
+  }
+  steps{
+    shell('''if ls / | grep task6_devops
+then
+sudo cp -rf * /task6_devops
+else
+sudo mkdir /task6_devops
+sudo cp -rf * /task6_devops
+fi  
+''')
+  }
+}
